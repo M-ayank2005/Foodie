1bcc96a889ae855d89e041d3f540506ebc7a8620
Saumya Subham Mishra <sm025663@gmail.com>
2024-10-15 19:22:21 +0530

    Updated UI

--- src/app/LoginPage/page.jsx
+++ src/app/LoginPage/page.jsx
@@ -3,8 +3,10 @@ import AuthForm from "../auth/page";
 
 const Page = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-200">
-      <AuthForm />
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg sm:max-w-md">
+        <AuthForm />
+      </div>
     </div>
   );
 };
