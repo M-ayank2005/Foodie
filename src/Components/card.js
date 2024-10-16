e19f822d7ef38bf54ea392268c801f7591ea2f2b
Madan Karki <karkimadan80@gmail.com>
2024-10-16 14:00:56 +1100

    lazy load image in home page cards

--- src/Components/card.js
+++ src/Components/card.js
@@ -2,18 +2,17 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import  {LazyLoadImage } from "react-lazy-load-image-component";
 
 const Card = ({ imageSrc, title, description, link }) => {
   return (
     <Link href={link} legacyBehavior>
       <a className="block w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg hover:shadow-2xl transform hover:scale-105 transition-transform duration-300">
-        <Image
-          src={imageSrc}
-          alt={title}
-          width={400}
-          height={250}
-          className="w-full h-48 object-cover"
-        />
+        <LazyLoadImage src={imageSrc.src}
+                width={400} height={250}
+                alt={title}
+                className="w-full h-48 object-cover"
+              />
         <div className="p-4">
           <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
           <p className="mt-2 text-gray-600">{description}</p>
