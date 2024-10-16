2e2d1f7874ede8ef287f65abda8e5ea98614a32d
Madan Karki <karkimadan80@gmail.com>
2024-10-16 13:43:28 +1100

    lazy loading added to menu page

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,7 +1,9 @@
+'use client';
 import React from 'react';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/footer';
 import Image from 'next/image';
+import  {LazyLoadImage } from "react-lazy-load-image-component";
 
 const menuItems = [
   { 
@@ -75,7 +77,11 @@ const Menu = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {menuItems.map((item, index) => (
             <div key={index} className="bg-gray-300 rounded-lg shadow-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-              <Image src={item.image} alt={item.name} width={50} height={50} className="w-full h-48 object-cover" />
+              <LazyLoadImage src={item.image}
+                width={50} height={50}
+                alt={item.name}
+                className="w-full h-48 object-cover"
+              />
               <div className="p-6">
                 <h2 className="text-2xl font-bold mb-2">{item.name}</h2>
                 <p className="text-gray-700 mb-4">{item.description}</p>
