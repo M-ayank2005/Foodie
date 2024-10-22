bf66afcc9ed956822aeffd56e3faed481519da13
anurag2787 <anuragyadav2787@gmail.com>
2024-10-22 17:35:27 +0530

    Improves UI of Contact Us Page fixes some minor issue and added feature of email reciveing whenver user fill the form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,9 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function ContactUs() {
+  const form = useRef();
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,65 +19,82 @@ function ContactUs() {
     });
   };
 
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    emailjs
+      .sendForm('service_bccq7bb', 'template_kkx1cqr', form.current, 'Gfu8aXmYc3toi7THG')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
     setSubmitted(true);
   };
+  
 
   return (
     <div className="flex flex-col items-center my-6 justify-center min-h-screen p-4">
-      <div className=" rounded-xl p-8 max-w-4xl w-full text-center bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
-        <h1 className="text-4xl font-extrabold mb-6">
+      <div className=" rounded-xl shadow-2xl p-8 max-w-4xl w-full text-center bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
+        <h1 className={` font-extrabold mb-6  ${submitted ? 'text-5xl' : 'text-4xl'}`}>
           Contact Us
         </h1>
-        <p className="text-lg mb-6 text-gray-600 dark:text-gray-300">
-          We would love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out to us.
-        </p>
+
         {!submitted ? (
-          <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-4">
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder="Your Name"
-              className="w-full p-3 rounded-lg bg-gray-50 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
+          <>
+            <p className="text-lg mb-6 text-gray-600 dark:text-gray-300">
+              We would love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out to us.
+            </p>
+            <form ref={form} onSubmit={handleSubmit} className="flex flex-col items-center space-y-4">
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Enter Your Name"
+                className="w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
                        placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
                        transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500"
-              required
-            />
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Your Email"
-              className="w-full p-3 rounded-lg bg-gray-50 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter Your Email"
+                className="w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
                        placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
                        transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500"
-              required
-            />
-            <textarea
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              placeholder="Your Message"
-              rows="4"
-              className="w-full p-3 rounded-lg bg-gray-50 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
+                required
+              />
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                placeholder="Entery Your Message"
+                rows="4"
+                className=" w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
                        placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
                        transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500"
-              required
-            />
-            <button
-              type="submit"
-              className="px-8 py-3 rounded-lg text-lg font-bold text-white bg-green-600 hover:bg-green-700 
+                required
+              />
+              <button
+                type="submit"
+                className="px-8 py-3 rounded-lg text-lg font-bold text-white bg-green-600 hover:bg-green-700 
                        dark:bg-green-700 dark:hover:bg-green-800 shadow-md transition duration-300"
-            >
-              Send Message
-            </button>
-          </form>
+              >
+                Send Message
+              </button>
+            </form>
+          </>
+
         ) : (
-          <p className="text-lg mt-6 text-gray-900 dark:text-white">
+          <p className="text-3xl p-28 mt-6 text-gray-900 dark:text-white">
             Thank you for your message! We will get back to you soon.
           </p>
         )}
