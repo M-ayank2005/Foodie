42eaf028bd646f2059be7ca5b429d5583e07bf91
Tanishq Aswar <tanishqaswar2005@gmail.com>
2024-10-31 15:51:06 +0530

    Menu Categorisation Feature Added

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,8 @@
 'use client';
 import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import Footer from '../../Components/Footer'
+
 
 function ContactUs() {
   const form = useRef();
@@ -38,80 +40,104 @@ function ContactUs() {
   
 
   return (
-    <div className="flex flex-col items-center my-6 justify-center min-h-screen p-4">
-      <div className=" rounded-xl shadow-2xl p-8 max-w-4xl w-full text-center bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
-        <h1 className={` font-extrabold mb-6  ${submitted ? 'text-5xl' : 'text-4xl'}`}>
-          Contact Us
-        </h1>
+    <>
+      <div className='flex flex-col items-center my-6 justify-center min-h-screen p-4'>
+        <div className=' rounded-xl shadow-2xl p-8 max-w-4xl w-full text-center bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white'>
+          <h1
+            className={` font-extrabold mb-6  ${
+              submitted ? 'text-5xl' : 'text-4xl'
+            }`}
+          >
+            Contact Us
+          </h1>
 
-        {!submitted ? (
-          <>
-            <p className="text-lg mb-6 text-gray-600 dark:text-gray-300">
-              We would love to hear from you! Whether you have questions, feedback, or just want to say hello, feel free to reach out to us.
-            </p>
-            <form ref={form} onSubmit={handleSubmit} className="flex flex-col items-center space-y-4">
-              <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                placeholder="Enter Your Name"
-                className="w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
-                       placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
-                       transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500"
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Enter Your Email"
-                className="w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
-                       placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
-                       transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500"
-                required
-              />
-              <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                placeholder="Entery Your Message"
-                rows="4"
-                className=" w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
-                       placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
-                       transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500"
-                required
-              />
-              <button
-                type="submit"
-                className="px-8 py-3 rounded-lg text-lg font-bold text-white bg-green-600 hover:bg-green-700 
-                       dark:bg-green-700 dark:hover:bg-green-800 shadow-md transition duration-300"
+          {!submitted ? (
+            <>
+              <p className='text-lg mb-6 text-gray-600 dark:text-gray-300'>
+                We would love to hear from you! Whether you have questions,
+                feedback, or just want to say hello, feel free to reach out to
+                us.
+              </p>
+              <form
+                ref={form}
+                onSubmit={handleSubmit}
+                className='flex flex-col items-center space-y-4'
               >
-                Send Message
-              </button>
-            </form>
-          </>
-
-        ) : (
-          <p className="text-3xl p-28 mt-6 text-gray-900 dark:text-white">
-            Thank you for your message! We will get back to you soon.
-          </p>
-        )}
-        <div className="mt-8">
-          <p className="text-xl font-semibold mb-2 text-gray-900 dark:text-white">
-            Additional Contact Information
-          </p>
-          <p className="text-md mb-2">
-            Email: <a href="mailto:support@foodie.com" className="text-blue-600 dark:text-blue-400 hover:underline">support@foodie.com</a>
-          </p>
-          <p className="text-md">
-            Twitter: <a href="https://twitter.com/foodie" className="text-blue-600 dark:text-blue-400 hover:underline">Twitter@foodie</a>
-          </p>
+                <input
+                  type='text'
+                  name='name'
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder='Enter Your Name'
+                  className='w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
+                       placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
+                       transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500'
+                  required
+                />
+                <input
+                  type='email'
+                  name='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder='Enter Your Email'
+                  className='w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
+                       placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
+                       transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500'
+                  required
+                />
+                <textarea
+                  name='message'
+                  value={formData.message}
+                  onChange={handleChange}
+                  placeholder='Entery Your Message'
+                  rows='4'
+                  className=' w-full p-3 rounded-lg bg-gray-200 dark:bg-gray-700 border border-gray-200 dark:border-gray-600 
+                       placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white 
+                       transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-500'
+                  required
+                />
+                <button
+                  type='submit'
+                  className='px-8 py-3 rounded-lg text-lg font-bold text-white bg-green-600 hover:bg-green-700 
+                       dark:bg-green-700 dark:hover:bg-green-800 shadow-md transition duration-300'
+                >
+                  Send Message
+                </button>
+              </form>
+            </>
+          ) : (
+            <p className='text-3xl p-28 mt-6 text-gray-900 dark:text-white'>
+              Thank you for your message! We will get back to you soon.
+            </p>
+          )}
+          <div className='mt-8'>
+            <p className='text-xl font-semibold mb-2 text-gray-900 dark:text-white'>
+              Additional Contact Information
+            </p>
+            <p className='text-md mb-2'>
+              Email:{' '}
+              <a
+                href='mailto:support@foodie.com'
+                className='text-blue-600 dark:text-blue-400 hover:underline'
+              >
+                support@foodie.com
+              </a>
+            </p>
+            <p className='text-md'>
+              Twitter:{' '}
+              <a
+                href='https://twitter.com/foodie'
+                className='text-blue-600 dark:text-blue-400 hover:underline'
+              >
+                Twitter@foodie
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+      <Footer />
+    </>
+  )
 }
 
 export default ContactUs;
--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,96 +1,160 @@
-"use client"
+'use client'
 
-
-import React from 'react';
-import Footer from '../../Components/Footer';
-import Section from '@/Components/Section';
-import Image from 'next/image';
+import React, { useState } from 'react'
+import Footer from '../../Components/Footer'
+import Section from '@/Components/Section'
+import Image from 'next/image'
+import { useDarkMode } from '../DarkModeContext'
 
 const menuItems = [
-  { 
-    name: 'Margherita Pizza', 
-    price: 'Rs12.99', 
-    description: 'Classic pizza with tomato sauce, mozzarella, and basil.', 
-    image: 'https://images.unsplash.com/photo-1598023696416-0193a0bcd302?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8TWFyZ2hlcml0YSUyMFBpenphfGVufDB8fDB8fHww' 
+  {
+    name: 'Margherita Pizza',
+    category: 'Pizza',
+    price: 'Rs12.99',
+    description: 'Classic pizza with tomato sauce, mozzarella, and basil.',
+    image:
+      'https://images.unsplash.com/photo-1598023696416-0193a0bcd302?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8TWFyZ2hlcml0YSUyMFBpenphfGVufDB8fDB8fHww',
   },
-  { 
-    name: 'Pepperoni Pizza', 
-    price: 'Rs14.99', 
-    description: 'Pepperoni, mozzarella, and tomato sauce.', 
-    image: 'https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8UGVwcGVyb25pJTIwUGl6emF8ZW58MHx8MHx8fDA%3D' 
+  {
+    name: 'Pepperoni Pizza',
+    category: 'Pizza',
+    price: 'Rs14.99',
+    description: 'Pepperoni, mozzarella, and tomato sauce.',
+    image:
+      'https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8UGVwcGVyb25pJTIwUGl6emF8ZW58MHx8MHx8fDA%3D',
   },
-  { 
-    name: 'BBQ Chicken Pizza', 
-    price: 'Rs15.99', 
-    description: 'BBQ chicken, red onions, and cilantro.', 
-    image: 'https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fEJCUSUyMENoaWNrZW4lMjBQaXp6YXxlbnwwfHwwfHx8MA%3D%3D' 
+  {
+    name: 'BBQ Chicken Pizza',
+    category: 'Pizza',
+    price: 'Rs15.99',
+    description: 'BBQ chicken, red onions, and cilantro.',
+    image:
+      'https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fEJCUSUyMENoaWNrZW4lMjBQaXp6YXxlbnwwfHwwfHx8MA%3D%3D',
   },
-  { 
-    name: 'Veggie Pizza', 
-    price: 'Rs13.99', 
-    description: 'Bell peppers, olives, onions, and mushrooms.', 
-    image: 'https://images.unsplash.com/photo-1617470702892-e01504297e84?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8VmVnZ2llJTIwUGl6emF8ZW58MHx8MHx8fDA%3D' 
+  {
+    name: 'Veggie Pizza',
+    category: 'Pizza',
+    price: 'Rs13.99',
+    description: 'Bell peppers, olives, onions, and mushrooms.',
+    image:
+      'https://images.unsplash.com/photo-1617470702892-e01504297e84?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8VmVnZ2llJTIwUGl6emF8ZW58MHx8MHx8fDA%3D',
   },
-  { 
-    name: 'Caesar Salad', 
-    price: 'Rs9.99', 
-    description: 'Romaine lettuce, Caesar dressing, and croutons.', 
-    image: 'https://images.unsplash.com/photo-1551024601-bec78aea704b' 
+  {
+    name: 'Caesar Salad',
+    category: 'Salad',
+    price: 'Rs9.99',
+    description: 'Romaine lettuce, Caesar dressing, and croutons.',
+    image: 'https://images.unsplash.com/photo-1551024601-bec78aea704b',
   },
-  { 
-    name: 'Greek Salad', 
-    price: 'Rs10.99', 
-    description: 'Mixed greens, feta cheese, olives, and vinaigrette.', 
-    image: 'https://images.unsplash.com/photo-1625944230945-1b7dd3b949ab?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Z3JlZWslMjBzYWxhZHxlbnwwfHwwfHx8MA%3D%3D' 
+  {
+    name: 'Greek Salad',
+    category: 'Salad',
+    price: 'Rs10.99',
+    description: 'Mixed greens, feta cheese, olives, and vinaigrette.',
+    image:
+      'https://images.unsplash.com/photo-1625944230945-1b7dd3b949ab?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Z3JlZWslMjBzYWxhZHxlbnwwfHwwfHx8MA%3D%3D',
   },
-  { 
-    name: 'Spaghetti Bolognese', 
-    price: 'Rs14.99', 
-    description: 'Spaghetti with a rich meat sauce.', 
-    image: 'https://images.unsplash.com/photo-1589301760014-d929f3979dbc' 
+  {
+    name: 'Spaghetti Bolognese',
+    category: 'Pasta',
+    price: 'Rs14.99',
+    description: 'Spaghetti with a rich meat sauce.',
+    image: 'https://images.unsplash.com/photo-1589301760014-d929f3979dbc',
   },
-  { 
-    name: 'Fettuccine Alfredo', 
-    price: 'Rs13.99', 
-    description: 'Fettuccine pasta with creamy Alfredo sauce.', 
-    image: 'https://images.unsplash.com/photo-1551218808-94e220e084d2' 
+  {
+    name: 'Fettuccine Alfredo',
+    category: 'Pasta',
+    price: 'Rs13.99',
+    description: 'Fettuccine pasta with creamy Alfredo sauce.',
+    image: 'https://images.unsplash.com/photo-1551218808-94e220e084d2',
   },
-  { 
-    name: 'Chicken Parmesan', 
-    price: 'Rs16.99', 
-    description: 'Breaded chicken breast with marinara and cheese.', 
-    image: 'https://images.unsplash.com/photo-1632778149955-e80f8ceca2e8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q2hpY2tlbiUyMFBhcm1lc2FufGVufDB8fDB8fHww' 
+  {
+    name: 'Chicken Parmesan',
+    category: 'Entree',
+    price: 'Rs16.99',
+    description: 'Breaded chicken breast with marinara and cheese.',
+    image:
+      'https://images.unsplash.com/photo-1632778149955-e80f8ceca2e8?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q2hpY2tlbiUyMFBhcm1lc2FufGVufDB8fDB8fHww',
   },
-  { 
-    name: 'Tiramisu', 
-    price: 'Rs6.99', 
-    description: 'Classic Italian dessert with coffee and mascarpone.', 
-    image: 'https://images.unsplash.com/photo-1631206753348-db44968fd440?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fFRpcmFtaXN1fGVufDB8fDB8fHww' 
+  {
+    name: 'Tiramisu',
+    category: 'Dessert',
+    price: 'Rs6.99',
+    description: 'Classic Italian dessert with coffee and mascarpone.',
+    image:
+      'https://images.unsplash.com/photo-1631206753348-db44968fd440?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fFRpcmFtaXN1fGVufDB8fDB8fHww',
   },
-];
+]
+
+const categories = [...new Set(menuItems.map((item) => item.category)), 'All']
 
 const Menu = () => {
+  const [selectedCategory, setSelectedCategory] = useState('All')
+
+  const filteredItems =
+    selectedCategory === 'All'
+      ? menuItems
+      : menuItems.filter((item) => item.category === selectedCategory)
+
+  const { darkMode } = useDarkMode();
+
   return (
-    <main className="min-h-screen flex flex-col">
+    <main className='min-h-screen flex flex-col'>
       <Section>
-        <h1 className="text-6xl text-center font-bold tracking-wide mb-12">Our Menu</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {menuItems.map((item, index) => (
-            <div key={index} className="bg-gray-300 rounded-lg shadow-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105">
-              <Image src={item.image} alt={item.name} width={50} height={50} className="w-full h-48 object-cover" />
-              <div className="p-6">
-                <h2 className="text-2xl text-black font-bold mb-2">{item.name}</h2>
-                <p className="text-gray-700 mb-4">{item.description}</p>
-                <p className="text-lg font-semibold text-blue-600">{item.price}</p>
+        <h1 className='text-6xl text-center font-bold tracking-wide mb-12'>
+          Our Menu
+          <span
+            className={`blinking-underscore ${
+              darkMode ? 'text-white' : 'text-black'
+            }`}
+          >
+            _
+          </span>
+        </h1>
+        <div className='flex justify-center mb-8'>
+          {categories.map((category) => (
+            <button
+              key={category}
+              className={`px-4 py-2 rounded-full mr-2 ${
+                selectedCategory === category
+                  ? 'bg-blue-600 text-white'
+                  : 'bg-gray-200 text-gray-700'
+              }`}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          {filteredItems.map((item, index) => (
+            <div
+              key={index}
+              className='bg-gray-300 rounded-lg shadow-lg overflow-hidden transition duration-300 ease-in-out transform hover:scale-105'
+            >
+              <Image
+                src={item.image}
+                alt={item.name}
+                width={50}
+                height={50}
+                className='w-full h-48 object-cover'
+              />
+              <div className='p-6'>
+                <h2 className='text-2xl text-black font-bold mb-2'>
+                  {item.name}
+                </h2>
+                <p className='text-gray-700 mb-4'>{item.description}</p>
+                <p className='text-lg font-semibold text-blue-600'>
+                  {item.price}
+                </p>
               </div>
             </div>
           ))}
         </div>
-
       </Section>
       <Footer />
     </main>
-  );
-};
+  )
+}
 
-export default Menu;
+export default Menu
